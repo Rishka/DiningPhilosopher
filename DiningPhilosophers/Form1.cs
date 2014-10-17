4773732329ec0a65044aa7fcaf6841768bb315a6
Adam Perlman <aperlman@me.com>
2014-10-17 11:57:08 -0400

    Start implementing Philosopher class

--- DiningPhilosophers/Form1.cs
+++ DiningPhilosophers/Form1.cs
@@ -12,9 +12,45 @@ namespace DiningPhilosophers
 {
     public partial class Form1 : Form
     {
+        public const int NUM_PHIL = 5;
         public Form1()
         {
             InitializeComponent();
+            forks = new List(NUM_PHIL);
+            var philosophers = new List<Philosopher>();
+            for (int i =0; i< NUM_PHIL; i++;)
+                    philosophers.add(new Philosopher(i, NUM_PHIL));
         }
     }
+    public class Philosopher
+    {
+            private int seat, rightFork, leftFork;
+            private bool hasRight { get; set; }
+            private bool hasLeft { get; set; }
+            public static List forks;
+            public Philosopher(int seat, int num_phil)
+            {
+                    this.seat = seat;
+                    rightFork = seatNumber;
+                    leftFork = seatNumber % num_phil + 1;
+                    hasLeft = false;
+                    hasRight = false;
+            }
+            public bool Dine()
+            {
+                    if (hasLeft && hasRight)
+                            return true;
+                    else
+                            return false;
+            }
+           
+    }
 }
+
+
+
+
+
+
+
+
