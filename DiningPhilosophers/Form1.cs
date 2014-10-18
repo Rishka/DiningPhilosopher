f0dca2144ee1555810bbd6f53d3c01925bfdcdb0
Adam Perlman <aperlman@me.com>
2014-10-18 15:04:00 -0400

    Modified solution to utilize multiple threads

--- DiningPhilosophers/Form1.cs
+++ DiningPhilosophers/Form1.cs
@@ -7,12 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Random;
 
 namespace DiningPhilosophers
 {
     public partial class Form1 : Form
     {
         public const int NUM_PHIL = 5;
+        public List forks;
+        private cont;
         public Form1()
         {
             InitializeComponent();
@@ -20,14 +23,29 @@ namespace DiningPhilosophers
             var philosophers = new List<Philosopher>();
             for (int i =0; i< NUM_PHIL; i++;)
                     philosophers.add(new Philosopher(i, NUM_PHIL));
+            Parrallel.foreach (var phil in philosophers) //runs each philiosopher in his own thread
+                    {
+                            while(cont)//continue until user ends the program
+                                    {
+                                            System.Threading.Thread.Sleep(Random.Next(0, 10000));//Think from 0 to 10 seconds
+                                            lock(forks.get(phil.leftFork))
+                                                    {
+                                                            lock(forks.get(phil.rightFork))
+                                                                    {
+                                                                            Console.Writeline("Philosopher {0} is eating" , phil.seat.ToString()})
+                                                                    }
+                                                    }
+                                    }
+                    }
         }
     }
     public class Philosopher
     {
-            private int seat, rightFork, leftFork;
+            public int seat, rightFork, leftFork;
             private bool hasRight { get; set; }
             private bool hasLeft { get; set; }
-            public static List forks;
+            public double timeThinking;
+            private string status { get; set; }
             public Philosopher(int seat, int num_phil)
             {
                     this.seat = seat;
@@ -35,6 +53,7 @@ namespace DiningPhilosophers
                     leftFork = seatNumber % num_phil + 1;
                     hasLeft = false;
                     hasRight = false;
+                    status = "Thinking"
             }
             public bool Dine()
             {
